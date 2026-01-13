1ec69cd63da35689ff78f27482f6c3a3533db92f
Matt Oestreich <matthewpoestreich@gmail.com>
2026-01-13 14:21:49 -0600

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Leaf Venation
+
+Implementation of ["Modeling and visualization of leaf venation patterns" paper](https://dl.acm.org/doi/10.1145/1073204.1073251).
+
+![leaf_venation_screenshot](./leaf-venation.png)
+
+# Quick Start
+
+```
+cargo run
+```
+
+Then press the `space` key to watch veins grow!
---
+++ src/main.rs
@@ -0,0 +1,161 @@
+use raylib::{RaylibHandle, color::Color, ffi::KeyboardKey, math::Vector2, prelude::RaylibDraw};
+
+const VEIN_RADIUS: f32 = 5.0;
+const VEIN_COLOR: Color = Color::WHITE;
+const VEIN_CORE_COLOR: Color = Color::BLACK;
+const AUXINS_RATE: usize = 20;
+const AUXINS_RADIUS: f32 = 5.0; // Should be same as VEIN_RADIUS
+const AUXINS_COLOR: Color = Color::RED;
+const AUXIMITY: f32 = 30.0;
+
+fn main() {
+    assert_eq!(
+        VEIN_RADIUS, AUXINS_RADIUS,
+        "VEIN_RADIUS ({VEIN_RADIUS}) and AUXINS_RADIUS ({AUXINS_RADIUS}) should be equal!",
+    );
+
+    let width = 800;
+    let height = 600;
+
+    let (mut rl, rl_thread) = raylib::init()
+        .size(width, height)
+        .title("Leaf Venation")
+        .build();
+
+    let mut vein = Vein::new();
+    vein.position.x = (width / 2) as f32;
+    vein.position.y = (height * 2 / 3) as f32;
+
+    let mut veins = vec![vein];
+    let mut auxins = vec![];
+
+    spray_auxins(&rl, &mut auxins);
+    kill_auxins_by_auximity(&mut auxins, &mut veins);
+
+    while !rl.window_should_close() {
+        if rl.is_key_pressed(KeyboardKey::KEY_SPACE) {
+            calc_growth_dir(&mut auxins, &mut veins);
+            grow_new_veins(&mut veins);
+            kill_auxins_by_auximity(&mut auxins, &mut veins);
+            spray_auxins(&rl, &mut auxins);
+            kill_auxins_by_auximity(&mut auxins, &mut veins);
+        }
+
+        let mut drawing = rl.begin_drawing(&rl_thread);
+
+        drawing.set_target_fps(60);
+        drawing.clear_background(Color::get_color(0x181818FF));
+
+        for vein in veins.iter_mut() {
+            drawing.draw_circle(
+                vein.position.x as i32,
+                vein.position.y as i32,
+                VEIN_RADIUS,
+                VEIN_COLOR,
+            );
+            drawing.draw_circle(
+                vein.position.x as i32,
+                vein.position.y as i32,
+                VEIN_RADIUS / 2f32,
+                VEIN_CORE_COLOR,
+            );
+            vein.direction.scale(20f32);
+            drawing.draw_line_v(vein.position, vein.position + vein.direction, Color::PURPLE);
+        }
+
+        for auxin in auxins.iter() {
+            drawing.draw_circle(auxin.x as i32, auxin.y as i32, AUXINS_RADIUS, AUXINS_COLOR);
+        }
+    }
+}
+
+struct Vein {
+    position: Vector2,
+    direction: Vector2,
+}
+
+impl Vein {
+    fn new() -> Self {
+        Self {
+            position: Vector2::default(),
+            direction: Vector2::default(),
+        }
+    }
+}
+
+fn remove_unordered<T>(vec: &mut Vec<T>, index: usize) {
+    let last = vec.len() - 1;
+    vec.swap(index, last);
+    vec.pop();
+}
+
+fn spray_auxins(rl: &RaylibHandle, auxins: &mut Vec<Vector2>) {
+    let height = rl.get_screen_height();
+    let width = rl.get_screen_width();
+
+    for _ in 0..AUXINS_RATE {
+        let x = rl.get_random_value::<i32>(0..width.saturating_sub(1)) as f32;
+        let y = rl.get_random_value::<i32>(0..height.saturating_sub(1)) as f32;
+        auxins.push(Vector2::new(x, y));
+    }
+}
+
+fn kill_auxins_by_auximity(auxins: &mut Vec<Vector2>, veins: &mut [Vein]) {
+    let mut to_remove = vec![];
+
+    for (index, auxin) in auxins.iter().enumerate() {
+        for vein in veins.iter() {
+            if auxin.distance_to(vein.position) <= AUXIMITY {
+                to_remove.push(index);
+                break;
+            }
+        }
+    }
+
+    for &i in to_remove.iter().rev() {
+        remove_unordered(auxins, i);
+    }
+}
+
+fn calc_growth_dir(auxins: &mut [Vector2], veins: &mut [Vein]) {
+    if veins.is_empty() {
+        println!("calc_growth_dir : veins is empty!");
+        return;
+    }
+
+    veins.iter_mut().for_each(|vein| {
+        vein.direction = Vector2::zero();
+    });
+
+    for auxin in auxins.iter() {
+        let mut closest = 0usize;
+        for (i, vein) in veins.iter().enumerate() {
+            if vein.position.distance_to(*auxin) < veins[closest].position.distance_to(*auxin) {
+                closest = i;
+            }
+        }
+        veins[closest].direction += *auxin - veins[closest].position;
+    }
+
+    veins.iter_mut().for_each(|vein| vein.direction.normalize());
+}
+
+fn grow_new_veins(veins: &mut Vec<Vein>) {
+    let mut new_veins_position = Vec::<Vector2>::new();
+
+    for vein in veins.iter_mut() {
+        if vein.direction.x == 0f32 && vein.direction.y == 0f32 {
+            continue;
+        }
+        let x = vein.position.x + vein.direction.x * VEIN_RADIUS * 2f32;
+        let y = vein.position.y + vein.direction.y * VEIN_RADIUS * 2f32;
+        let vein_position = Vector2::new(x, y);
+        new_veins_position.push(vein_position);
+    }
+
+    for position in new_veins_position.iter_mut() {
+        let mut new_vein = Vein::new();
+        new_vein.position = *position;
+        veins.push(new_vein);
+    }
+}
